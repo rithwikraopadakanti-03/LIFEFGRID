533e25967a9c501ce302bfb22fde5bbe5a78c744
rithwikraopadakanti-03 <rithwikraopadakanti@gmail.com>
2026-08-01 17:10:43 +0530

    Configure Vercel frontend and Render backend deployment files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 import App from './App.jsx'
 import './index.css'
+
+const apiBase = import.meta.env.VITE_API_URL || ''
+if (apiBase) {
+  axios.defaults.baseURL = apiBase
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
